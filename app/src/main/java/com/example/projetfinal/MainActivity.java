4537632259d1621c9722591f21bfa5b68e1ed380
Julien Forget <julius.forgettus@gmail.com>
2021-12-07 01:14:04 -0500

    gestion sauvegarde

--- app/src/main/java/com/example/projetfinal/MainActivity.java
+++ app/src/main/java/com/example/projetfinal/MainActivity.java
@@ -10,22 +10,60 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-    private int m_argent;
+    private Integer m_argent;
     private int m_multiplier;
     private int m_clickValue;
     private int[] etatUpgrades;
+    private String FNAME;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FNAME = "sauvegardeClicker.txt";
+
+        FileInputStream fis;
+        try {
+            fis = openFileInput(FNAME);
+            byte[] buffer = new byte[1024];
+            StringBuilder content = new StringBuilder();
+            while ((fis.read(buffer)) !=
+                    -1) {
+                content.append(new String(buffer));
+            }
+
+            String contenu = content.toString();
+            Pattern p = Pattern.compile(" ");
+            String quantite[] = contenu.split(p.pattern());
+            m_argent = Integer.parseInt(quantite[0]);
+            etatUpgrades = new int[7];
+            for (int i = 1; i < 8; i++)
+            {
+                etatUpgrades[i-1] = Integer.parseInt(quantite[i]);
+            }
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(m_argent == null)
+            m_argent = 0;
+        if(etatUpgrades == null)
+            etatUpgrades = new int[] {0,0,0,0,0,0,0};
         m_clickValue = 1;
         m_multiplier = 1;
-        m_argent = 0;
-        etatUpgrades = new int[] {0,0,0,0,0,0,0};
         buttonSwitch(R.layout.activity_main);
         ImageView buttonMain = findViewById(R.id.Clicker_main);
 
@@ -92,4 +130,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onStop()
+    {
+        FileOutputStream fos;
+        try {
+            fos = openFileOutput(FNAME, Context.MODE_PRIVATE);
+            fos.write(String.valueOf(m_argent).getBytes());
+
+            for (int i = 0; i < etatUpgrades.length; i++)
+            {
+                fos.write(" ".getBytes());
+                System.out.println(etatUpgrades[i]);
+                fos.write(String.valueOf(etatUpgrades[i]).getBytes());
+            }
+            fos.write(" buffer".getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        super.onStop();
+    }
 }
