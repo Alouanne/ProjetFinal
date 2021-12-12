c818b2b67e7a47c52e3085de2fcd7d8e19b19c92
Alouanne <aleanne.camire@gmail.com>
2021-12-12 11:36:26 -0500

    ça plante, mais j'ai essayer de laiser des comentaires pour t'aider

--- app/src/main/java/com/example/projetfinal/MainActivity.java
+++ app/src/main/java/com/example/projetfinal/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //
         FNAME = "sauvegardeClicker.txt";
         nbClickSecondes = 0;
         setContentView(R.layout.activity_main);
